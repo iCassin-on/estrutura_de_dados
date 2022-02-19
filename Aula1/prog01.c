e4af0a2594a45a068e7d18b389a1e782796aca5e
iCassin-on <iago.cassin@gmail.com>
2022-02-19 01:30:22 -0300

    primeiro commit

---
+++ Aula1/prog01.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int v[500];
+    int i;
+
+    srand(time(0));
+    v[0] = 7;
+    for (i=0 ; i<500 ; i++) 
+        v[i] = v[i-1] + rand() % 10;
+    
+    for (i=0 ; i<500 ; i++)
+        printf("%d ", v[i]);
+    printf("\n");
+
+    return 0;
+}
+
+// O programa vai pedir um numero ao usuario e informar se o numero encontra-se dentro do range setado.
